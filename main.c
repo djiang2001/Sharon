b4ae30a4ad26dff8f5e7579dec7399e68b3731c1
djiang2001 <djiang2@stuy.edu>
2018-12-04 19:10:38 -0500

    done

--- main.c
+++ main.c
@@ -9,32 +9,38 @@ int main(){
   key_t key;
   int shmid;
   char * data;
-  char * input;
+  char input[256];
+  char input2[256];
 
-  key = ftok("/home/somefile", 'R');
+  key = ftok("/somefile", 'R');
   shmid = shmget(key,200,0644 | IPC_CREAT);
   data = shmat(shmid,(void*)0,0);
   
-  if(data){
+  if(!data[0]){
     printf("Recently created shared memory \n");
   }else{
     printf("Shared content: %s \n", data);
   }
-
-  while(
-  printf("Do you want to change data (y/n)? \n");
-  scanf("%c", &input);
-
-  if(strcmp(input,"y")){
-    printf("Input your string \n");
-  }else{
-    printf("Do you want to delete data (y/n)? \n");
-    scanf("%c",&input);
-
-    if(strcmp(input,"y"){
-
-      }
-      
+    
+  printf("Do you want to change data? (y/n) \n");
+  fgets(input,256,stdin);
+    
+  if(!strcmp(input,"y\n")){
+    printf("Input your new string: \n");
+    fgets(input,200,stdin);
+    data[strlen(input) - 1] = 0;
+    strcpy(data,input);
   }
+  printf("Shared memory content: %s \n", data);
+  
+  printf("Do you want to delete a segment? (y/n) \n");
+  fgets(input2,256,stdin);
+  
+  if(!strcmp(input2,"y\n")){
+    shmdt(data);
+    shmctl(shmid, IPC_RMID, NULL);
+    printf("Segment deleted \n");
+  }
+
   return 0;
 }
